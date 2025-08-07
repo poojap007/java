92bef890750dde21acd4ec1d2eafecec39e66688
poojap007 <perisetlapooja3@gmail.com>
2025-08-07 12:21:58 +0530

    Add files via upload

---
+++ springboot20/EmployeeService/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/employee/hello")
+    public String hello() {
+        return "Hello from Employee Service!";
+    }
+}
